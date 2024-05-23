3ef1a386b149d7d2a3356c3b9121a9eb365409ec
MauroTorresGit <maurotorres2802@gmail.com>
2024-05-23 17:16:28 -0300

    sc

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const celulares = [
     {nombre: 'iPhone 14 pro max', precio: 800},
     {nombre: 'iPhone 15', precio: 900},
     {nombre: 'iPhone 15 pro max', precio: 1100},
-    {nombre: 'Samsung S20', precio: 500},
+    {nombre: 'Samsung Zflip', precio: 500},
     {nombre: 'Samsung S21', precio: 700},
     {nombre: 'Samsung S22', precio: 900},
     {nombre: 'Samsung S23', precio: 1200},
