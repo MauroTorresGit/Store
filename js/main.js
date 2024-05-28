4815d3afd3e0f525c2e0217d3c425dd4a0704350
MauroTorresGit <maurotorres2802@gmail.com>
2024-05-27 23:27:04 -0300

    Filtro en busqueda

--- js/main.js
+++ js/main.js
@@ -47,16 +47,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('search-button'); //selecciona el boton
     const searchInput = document.getElementById('search-input'); //selecciona el input
     const resultsContainer = document.getElementById('results-container'); 
+    const items = document.querySelectorAll('.item'); //para seleccionar todos los "item"
 
         function ejecutarBusqueda() {
-            const query = searchInput.value;
+            const query = searchInput.value.toLowerCase();
+            let resultadosEncontrados = false;
+
+            items.forEach (item => {
+                item.style.display = 'block'; //para que se vuelvan a mostrar los items ocultados
+                const itemName = item.getAttribute('data-name').toLowerCase().trim();
+                if (itemName.includes(query)) {
+                    item.computedStyleMap.display = 'block'; //muestra el item
+                    resultadosEncontrados =  true;
+                }
+                else {
+                    item.style.display = 'none'; //oculta el item
+                }
+            });
+
             if (query) {
-                resultsContainer.textContent = `Resultados de la búsqueda: ${query}`;
+                resultsContainer.textContent = resultadosEncontrados ? `Resultados de la búsqueda: ${query}` : 'No se encontraron resultados.';
             } else {
                 resultsContainer.textContent = 'Por favor, ingrese un término de búsqueda.';
             }
-        } 
-
+        }
+        
         searchButton.addEventListener('click', ejecutarBusqueda);
         searchInput.addEventListener('keypress', function(event) {
             if (event.key === 'Enter') {
